02908b2cfd314d6f8628f51bb9f3283144005f2b
Zubair Mazumder <zubairmazumder624@gmail.com>
2026-09-24 15:39:37 +0600

    initial commit

---
+++ src/main/java/com/fundaxis/FundAxisApplication.java
@@ -0,0 +1,13 @@
+package com.fundaxis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundAxisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundAxisApplication.class, args);
+    }
+
+}
